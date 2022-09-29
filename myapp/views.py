07c5201139a70680f0f1f10d397ed5c447291368
athulnanda123 <athulnanda123@gmail.com>
2022-09-29 20:45:02 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,11 @@
+
+from django.http import HttpResponse
+# Create your views here.
+
+
+def index(request):
+    d = {
+        "name": "Arun",
+        "age" : 30,
+    }
+    return HttpResponse("<b>Hello World</b>")
